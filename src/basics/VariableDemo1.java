8a58b6119abe591fd76dbd5a487329d75c3142e6
musharafaahel <musaahel@gmail.com>
2022-08-29 09:55:17 +0530

    Day 2

---
+++ src/basics/VariableDemo1.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class VariableDemo1 {
+	public static void main(String[] args) {
+		//Syntax : accessModifier dataType variableName = value;
+		int value1 = 1, value2 = 2;
+		int value3 = value1 *  value2 * value2;
+		System.out.println(value3);
+	}
+}
+ 
