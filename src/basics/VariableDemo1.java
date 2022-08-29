d19406b9bd261a1e0b42d0ae3c8dcd4bd35da003
musharafaahel <musaahel@gmail.com>
2022-08-29 09:55:29 +0530

    Day 2

--- src/basics/VariableDemo1.java
+++ src/basics/VariableDemo1.java
@@ -5,7 +5,7 @@ public class VariableDemo1 {
 		//Syntax : accessModifier dataType variableName = value;
 		int value1 = 1, value2 = 2;
 		int value3 = value1 *  value2 * value2;
-		System.out.println(value3);
+		System.out.println(value3 );
 	}
 }
  
