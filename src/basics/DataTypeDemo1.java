93bc328010926d30dc5b574257c54f068ba8adf0
musharafaahel <musaahel@gmail.com>
2022-08-29 10:09:24 +0530

    Day 2

---
+++ src/basics/DataTypeDemo1.java
@@ -0,0 +1,20 @@
+package basics;
+
+public class DataTypeDemo1 {
+
+	public static void main(String[] args) {
+		int maxInt = 2147483647;
+		int minInt = -2147483648;
+		
+		short maxShort = 32767;
+		short minShort = -32768;
+		
+		long maxLong = 9_223_372_036_854_775_807L;
+		long minLong = -9_223_372_036_854_775_807L;
+		
+		byte manByte = 127;
+		byte minByte = -128; 
+	}
+
+}
+  
